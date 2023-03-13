46d227614cb5fb4e1fe5e2d0e2e58d724884157e
Rahul <90383484+Boogiieman@users.noreply.github.com>
2023-03-13 14:16:34 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,11 @@
+class program2
+{
+	public static void main(String[]arg)
+	{
+		int a=20,b=80;
+		if(b>a)
+			System.out.println("b is greater");
+		else
+			System.out.println("a is greater");
+	}
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+class sum
+{
+	public static void main(String[]arg)
+	{
+		double a=20,b=80,c;
+		c = a + b;
+			System.out.println(c);
+	
+	}
+}
